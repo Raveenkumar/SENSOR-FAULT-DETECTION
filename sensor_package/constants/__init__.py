1827f880e4cc8ffecb0b7a274197df165a9b756a
Raveenkumar <jraveen1997@gmail.com>
2024-08-27 21:24:16 +0530

    schema files updated (DSA)

---
+++ sensor_package/constants/__init__.py
@@ -0,0 +1,6 @@
+# validation constants
+REGEX_PATTERN :str = "wafer_(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[0-2])\d{4}_(0[0-9]|1[0-9]|2[0-3])([0-5][0-9]){2}\.csv"   # type: ignore
+TRAINING_BATCH_FILES_PATH = "artifacts/Client_DB_Data/training_batch_files"
+PREDICTION_BATCH_FILES_PATH = "artifacts/Client_DB_Data/prediction_batch_files"
+
+
