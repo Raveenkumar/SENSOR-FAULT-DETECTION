605f9dfc47b0bbf3412bcf52a1c57c0b23ef42cc
Raveenkumar <jraveen1997@gmail.com>
2024-08-27 17:47:57 +0530

    project setup done!

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+from sensor_package.constants import *
+
+setup(
+    name = "SENSOR-FAULT-DETECTION",
+    version = "1.0",
+    author = "Jenna Raveenkumar",
+    author_email = "jraveen1997@gmail.com",
+    packages= find_packages()
+)
+
